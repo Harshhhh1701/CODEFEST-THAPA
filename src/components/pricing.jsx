c450572cfbf4f9e1bd5aa7e3876e24a4efc059a1
Harsh <valambeharsh1701@gmail.com>
2022-07-05 16:26:33 +0530

    loop used for pricing

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 
 const Pricing = () => {
+    const arr=new Array(1,2,3);
   return (
     <>
+        
         <div className="flex mt-20 bg-blue-200 mx-10 items-center rounded-md">
             <div className="flex space-x-8 w-full h-screen items-center pt-8 mx-8 ">
-                <div className="flex bg-white h-1/2 w-1/4 flex-col justify-center items-center">
-                    <div className="bg-slate-300 h-3 w-36 mx-24 mt-3">
+                {arr.map((item,id)=>(
+                    <div key={id} className="flex bg-white h-1/2 w-1/4 flex-col justify-center items-center">
+                    <div key={id} className="bg-slate-300 h-3 w-36 mx-24 mt-3">
 
                     </div>
-                    <div className="bg-slate-300 h-2 w-52 mx-16 mt-3"></div>
-                    <div className="bg-slate-300 h-2 w-52 mx-16 mt-3"></div>
-                    <div className="bg-slate-300 h-2 w-52 mx-16 mt-3"></div>
-                    <div className="bg-slate-200 h-32 w-52 mx-16 mt-5"></div>
-                    <div className="bg-slate-300 h-6 w-24 mx-32 mt-12 rounded-lg"></div>
+                    <div key={id} className="bg-slate-300 h-2 w-52 mx-16 mt-3"></div>
+                    <div key={id} className="bg-slate-300 h-2 w-52 mx-16 mt-3"></div>
+                    <div key={id} className="bg-slate-300 h-2 w-52 mx-16 mt-3"></div>
+                    <div key={id} className="bg-slate-200 h-32 w-52 mx-16 mt-5"></div>
+                    <div key={id} className="bg-slate-300 h-6 w-24 mx-32 mt-12 rounded-lg"></div>
 
                 </div>
-                <div className="flex bg-white h-1/2 w-1/4 flex-col justify-center items-center">
+                ))}
+                
+                {/* <div className="flex bg-white h-1/2 w-1/4 flex-col justify-center items-center">
                     <div className="bg-slate-300 h-3 w-36 mx-24 mt-3">
 
                     </div>
@@ -35,7 +40,7 @@ const Pricing = () => {
                     <div className="bg-slate-300 h-2 w-52 mx-16 mt-3"></div>
                     <div className="bg-slate-200 h-32 w-52 mx-16 mt-5"></div>
                     <div className="bg-white h-6 w-24 mx-32 mt-12 rounded-lg border-solid border-2 border-slate-400"></div>
-                </div>
+                </div> */}
             </div>
             <div className=' pt-8 text-lg font-sans font-semibold'>
                 <p>Our pricing plans</p>

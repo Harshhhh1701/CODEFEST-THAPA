5aa7c1eac4e500c7a4fbb41dc1f441aa492697fe
Nimish Kashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-07-05 11:06:17 +0530

    Fixes in animation

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,6 +1,6 @@
 const PrimaryButton = ({ children }) => {
     return (
-        <button className='bg-indigo-900 text-white px-10 py-4 rounded-xl'>{children}</button>
+        <button className='bg-indigo-900 text-white px-10 py-4 rounded-xl button'>{children}</button>
     )
 }
 
